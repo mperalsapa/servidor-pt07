0b1578cca348ad156113f42226e2e17dfcc57701
Marc Peral <m.peral@sapalomera.cat>
2022-12-15 16:50:42 +0100

    refactor: added function to return json

--- src/controllers/read_users.php
+++ src/controllers/read_users.php
@@ -6,25 +6,38 @@ include_once("src/internal/form_errors.php");
 include_once("src/internal/db/mysql.php");
 
 // funció que genera un array preparat per convertir-lo en json
+// @return string[]
 function makeUser($name, $email, $birthDate, $country): array
 {
     $userData = array("name" => $name, "email" => $email, "birthDate" => $birthDate, "country" => $country);
     return $userData;
 }
 
+function reponseUsers(array $users): void
+{
+    // afegim l'array d'usuaris a un "objecte" (array)
+    $data = array("users" => $users);
+    // indiquem al navegador que el que rebrà es JSON
+    header('Content-Type: application/json; charset=utf-8');
+    // convertim l'array d'usuaris en JSON
+    echo json_encode($data);
+    die();
+}
+
 // creem un array que omplirem amb els usuaris de la base de dades
 $users = array();
 // agafem els usuaris de la base de dades
 $queriedUsers = getUsers();
+
+// en cas de la query sigui null, retornem un array buit
+if (is_null($queriedUsers)) {
+    reponseUsers($users);
+}
+
 // afegirem cada usuari a l'array d'usuaris buida
 while ($user = $queriedUsers->fetch()) {
     $userData = makeUser($user["nom"], $user["correu"], $user["naixement"], $user["pais"]);
     array_push($users, $userData);
 }
 
-// afegim l'array d'usuaris a un "objecte" (array)
-$data = array("users" => $users);
-// indiquem al navegador que el que rebrà es JSON
-header('Content-Type: application/json; charset=utf-8');
-// convertim l'array d'usuaris en JSON
-echo json_encode($data);
+reponseUsers($users);
